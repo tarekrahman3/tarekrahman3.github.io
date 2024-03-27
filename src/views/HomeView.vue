9e0e81787b4417bca0c3445ba2ada8cea8a7fcb1
tarekrahman3 <md.tarek420@gmail.com>
2024-03-27 22:02:16 +0600

    tr

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,44 +27,7 @@
         </table>
       </div>
     </div>
-    <div class="pt-16">
-      <div class="text-center">
-        <p class="text-rose-200 text-3xl">{{ '{*}' }}</p>
-        <p class="text-rose-200 text-3xl">{{ '<contact me>' }}</p>
-        <p class="pt-5 text-2xl">Ready to start your next project or have questions about my services? Don't hesitate to reach out!</p>
-        <div class="w-6/12 mx-auto pb-10">
-          <form class="text-start" action="" method="post">
-            <div class="md:flex items-center mt-12">
-              <div class="w-full md:w-1/2 flex flex-col">
-                <label class="leading-none text-white text-lg">Name</label>
-                <input type="text" class="leading-none text-white p-3 text-xl focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded" />
-              </div>
-              <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
-                <label class="leading-none text-white text-lg">Email</label>
-                <input type="email" class="leading-none text-white p-3 text-xl focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"/>
-              </div>
-            </div>
-            <div class="md:flex items-center mt-8">
-              <div class="w-full flex flex-col">
-                <label class="leading-none text-white text-lg">Subject</label>
-                <input type="text" class="leading-none text-white p-3 text-xl focus:outline-none focus:border-blue-700 mt-4 border-0 bg-gray-800 rounded"/>
-              </div>
-            </div>
-            <div>
-              <div class="w-full flex flex-col mt-8">
-                <label class="leading-none text-white text-lg">Message</label>
-                <textarea type="text" class="h-40 leading-none text-white p-3 text-xl focus:outline-none focus:border-blue-700 mt-4 bg-gray-800 border-0 rounded"></textarea>
-              </div>
-            </div>
-            <div class="flex items-center justify-center w-full">
-              <button class="mt-9 leading-none text-white py-4 px-10 bg-blue-700 rounded hover:bg-blue-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
-                  Send message
-              </button>
-            </div>
-            </form>
-        </div>
-      </div>
-    </div>
+    
   </main>
 </template>
 
